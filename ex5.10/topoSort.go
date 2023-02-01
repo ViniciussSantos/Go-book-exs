22f7a35539343cdd2abf705b86b8264f7258b127
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-02-01 05:33:57 -0300

    ex 5.10

---
+++ ex5.10/topoSort.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+)
+
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", i+1, course)
+	}
+}
+
+func topoSort(m map[string][]string) []string {
+	var order []string
+	seen := make(map[string]bool)
+	var visitAll func(items []string)
+	var visitAllMaps func(items map[string]bool)
+
+	visitAllMaps = func(items map[string]bool) {
+		for item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	visitAll = func(items []string) {
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				visitAll(m[item])
+				order = append(order, item)
+			}
+		}
+	}
+
+	var keys = make(map[string]bool)
+	for key := range m {
+		keys[key] = true
+	}
+
+	visitAllMaps(keys)
+	return order
+}
