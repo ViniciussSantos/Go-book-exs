d50095df36d94b45ee526ae420e9af41a07e2805
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-02-12 13:22:29 -0300

    ex 6.1

---
+++ ex6.1/bitVector.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+//!+intset
+
+// An IntSet is a set of small non-negative integers.
+// Its zero value represents the empty set.
+type IntSet struct {
+	words []uint64
+}
+
+// Has reports whether the set contains the non-negative value x.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add adds the non-negative value x to the set.
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith sets s to the union of s and t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+//!-intset
+
+//!+string
+
+// String returns the set as a string of the form "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// solution
+func popcount(x uint64) int {
+	var pc [256]byte
+
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func (s *IntSet) Len() int {
+	count := 0
+	for _, word := range s.words {
+		count += popcount(word)
+
+	}
+	return count
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	s.words[word] &^= 1 << bit
+
+}
+
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+func (s *IntSet) Copy() *IntSet {
+	intSetCopy := new(IntSet)
+
+	intSetCopy.words = make([]uint64, len(s.words))
+
+	copy(intSetCopy.words, s.words)
+
+	return intSetCopy
+}
