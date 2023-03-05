188335cc57e2546d0be880b0e774260a724919ad
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-03-05 19:29:11 -0300

    init

---
+++ ex7.13/ast.go
@@ -0,0 +1,26 @@
+package main
+
+type Expr interface {
+	Eval(env Env) float64
+	Check(vars map[Var]bool) error
+	String() string
+}
+
+type Var string
+
+type literal float64
+
+type unary struct {
+	operation rune
+	x         Expr
+}
+
+type binary struct {
+	operation rune
+	x, y      Expr
+}
+
+type call struct {
+	function string
+	args     []Expr
+}
---
+++ ex7.13/check.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func (v Var) Check(vars map[Var]bool) error {
+	vars[v] = true
+	return nil
+}
+
+func (literal) Check(vars map[Var]bool) error {
+	return nil
+}
+
+func (u unary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-", u.operation) {
+		return fmt.Errorf("unexpected unary operation %q", u.operation)
+	}
+	return u.x.Check(vars)
+}
+
+func (b binary) Check(vars map[Var]bool) error {
+	if !strings.ContainsRune("+-*/", b.operation) {
+		return fmt.Errorf("unexpected binary operation %q", b.operation)
+	}
+	if err := b.x.Check(vars); err != nil {
+		return err
+	}
+	return b.y.Check(vars)
+}
+
+func (c call) Check(vars map[Var]bool) error {
+	arity, ok := numParams[c.function]
+	if !ok {
+		return fmt.Errorf("unknown function %q", c.function)
+	}
+	if len(c.args) != arity {
+		return fmt.Errorf("call to %s has %d args, want %d",
+			c.function, len(c.args), arity)
+	}
+	for _, arg := range c.args {
+		if err := arg.Check(vars); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+var numParams = map[string]int{"pow": 2, "sin": 1, "sqrt": 1}
