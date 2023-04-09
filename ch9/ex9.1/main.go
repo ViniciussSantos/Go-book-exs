239bbe89935c0d1b0f5243554474b7319b05d782
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2023-04-09 17:44:10 -0300

    ex 9.1

---
+++ ch9/ex9.1/main.go
@@ -0,0 +1,34 @@
+package bank
+
+var deposits = make(chan int) // send amount to deposit
+var balances = make(chan int) // receive balance
+var withdraws = make(chan int)
+
+func Deposit(amount int) { deposits <- amount }
+func Balance() int       { return <-balances }
+func Withdraw(amount int) bool {
+	if Balance() < amount {
+		return false
+	}
+
+	withdraws <- amount
+	return true
+
+}
+
+func teller() {
+	var balance int // balance is confined to teller goroutine
+	for {
+		select {
+		case amount := <-deposits:
+			balance += amount
+		case balances <- balance:
+		case amount := <-withdraws:
+			balance -= amount
+		}
+	}
+}
+
+func init() {
+	go teller() // start the monitor goroutine
+}
